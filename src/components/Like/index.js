ae000f664908ee6a64df23883a61901216c3c2c6
liyunpeng <liyunpeng@navinfo.com>
2020-01-29 21:34:00 +0800

    input输入框响应

--- src/components/Like/index.js
+++ src/components/Like/index.js
@@ -4,21 +4,33 @@ export default class Like extends Component {
     constructor(){
         super()
         this.state={
-            isLike:true
+            isLike:false
         }
     }
     //点击事件
     handleClick= () => {
-        // this.state.isLike=!this.state.isLike
-        this.setState({
-            isLike:!this.state.isLike
+        //this.state.isLike=!this.state.isLike
+        // this.setState({
+        //     isLike:!this.state.isLike
+        // })
+        //第二种写法,匿名函数，会传递上一次的state,props
+        this.setState((preState,props) => {
+            console.log("上一次的state")
+            console.log(preState)
+            return{
+                isLike:!preState.isLike
+            }
+        },
+        () => {
+            console.log("当前state")
+            console.log(this.state)
         })
     }
     render() {
         return (
             <div>
                 <span onClick={this.handleClick}>
-                {this.state.isLike?'取消🧡':'喜欢💛'}
+                {this.state.isLike?'喜欢🧡':'取消💛'}
                 </span>
             </div>
         )
--- src/components/TodoInput/index.js
+++ src/components/TodoInput/index.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 
 export default class TodoInput extends Component {
-   
+    constructor(){
+        super()
+        this.state={
+            inputValue:""
+        }
+    }
+    handleChange=(e ) => {
+        this.setState({
+            inputValue:e.currentTarget.value
+        })
+        console.log(e.currentTarget)
+
+    }
     render() {
         return (
             <div>
-    <input type="text"/><button>{this.props.btntitle}</button>
+    <input type="text" value={this.state.inputValue } onChange={this.handleChange} /><button>{this.props.btntitle}</button>
             </div>
         )
     }
